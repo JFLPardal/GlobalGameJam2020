9a095b4f896be3f324af5eea428694e0f9c9041e
Unknown <jflpardal@gmail.com>
2020-02-01 11:05:47 +0000

    Player movement and Rotation, controllers and Multiplayer
    * Controllers added for player to rotate and move character;
    * Implemented both functionalities;
    * Game supports up to 4 players at the same time;

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] int movementSpeed = 10;
+
+    private Vector2 movement;
+    private Vector3 rotation;
+
+    void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        Vector3 newMovement = new Vector3(-movement.y, 0, movement.x) * Time.deltaTime * movementSpeed;
+        transform.Translate(newMovement);
+    }
+
+    private void Rotate()
+    {
+        //Vector3 newRotation = new Vector3(0, rotation, 0);
+        transform.Rotate(rotation, Space.World);
+    }
+    void OnMove(InputValue input)
+    {
+        movement = input.Get<Vector2>();
+    }
+
+    void OnInteract()
+    {
+        print("interact");
+    }
+
+    void OnRotate(InputValue input)
+    {
+        rotation = new Vector3(0, input.Get<float>(), 0);
+    }
+}
